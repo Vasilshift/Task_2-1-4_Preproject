47edd971ec48cc412ec9c1b200685de126db4980
Evgeny <vasilkalin@gmail.com>
2021-06-20 15:56:42 +0200

    Work variant

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+//    private Needle7 needle7;
+
+//    @Autowired
+//    public Needle7(Needle7 needle7) {
+//        this.needle7 = needle7;
+//    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
